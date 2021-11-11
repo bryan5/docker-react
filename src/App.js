9994bca76574baa6a36eda55bc4fcd6daf94916d
bryan <bwbortner@gmail.com>
2021-11-10 19:06:19 -0700

    Change text

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <p>
-          All your base are belong to us.
+          All your base are belong to us!
         </p>
         <a
           className="App-link"
